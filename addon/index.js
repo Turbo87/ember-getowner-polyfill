358b1d7d3d0d863fa718913edb9a745ed193f714
Robert Jackson <rjackson@linkedin.com>
2016-12-06 21:23:50 -0500

    Make pure polyfill, remove addon modules.

--- addon/index.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-Ember.deprecate("ember-getowner-polyfill is now a true polyfill. Use Ember.getOwner directly instead of importing from ember-getowner-polyfill", false, {
-  id: "ember-getowner-polyfill.import"
-});
-
-export default Ember.getOwner;
